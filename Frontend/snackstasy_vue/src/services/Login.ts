9d703beacb9e0946d54e7a521b36937f13bd5cf0
Max <maxrubel8@gmail.com>
2026-03-08 19:51:26 +0100

    for daniel

--- Frontend/snackstasy_vue/src/services/Login.ts
+++ Frontend/snackstasy_vue/src/services/Login.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 // const BaseUrl = "https://cooking-recipe-web-production.up.railway.app/api";
-const BaseUrl = "http://localhost:8080/api";
+const BaseUrl = "http://localhost:5000/";
 
 export async function Logout(): Promise<void> {
   const request = await axios.post(`${BaseUrl}/login/logout`, { withCredentials: true });
