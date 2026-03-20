05ad652a042897d4fc05906244b3a57c878e9e6e
DerRUbelRollt <maxrubel8@gmail.com>
2026-03-20 15:31:27 +0100

    fix login bug, Stuck in Landing page and skip the login

--- Frontend/snackstasy_vue/src/services/Authentification.ts
+++ Frontend/snackstasy_vue/src/services/Authentification.ts
@@ -21,7 +21,7 @@ export async function initAuth() {
 export function useAuth() {
   return {
     user,
-    isAuthenticated: () => !!user.value,
+    isAuthenticated: () => user.value?.logged_in === true,
     isLoading,
   };
 }
